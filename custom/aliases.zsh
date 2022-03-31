b409e365b14a5ffd538731f6b1eca8b17cf101a3
Thomas Hexton <thomashexton@sentia.com.au>
2022-04-01 10:45:56 +1100

    Remove some OF aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,33 +3,21 @@ alias ibrew='arch -x86_64 /usr/local/bin/brew'
 
 # Rails
 alias be="bundle exec"
-alias rick="bundle exec rails s webrick"
 alias rs="bundle exec rails s"
 alias rc="bundle exec rails c"
-alias spst="spring stop"
 alias dbmigrate="bundle exec rake db:migrate"
 alias dbrollback="bundle exec rake db:rollback"
 alias db:reset="bundle exec rake db:reset"
 alias db:reset:test="bundle exec rake db:reset RAILS_ENV=test"
 alias rspec="bundle exec rspec"
-# alias kill-everything="pkill -9 -f 'rb-fsevent|rails|spring|puma'"
 alias gqlschema="bundle exec rake graphql:dump_schema"
 
 # Git
 alias gcfo="git config --list --show-origin"
-alias gcpr="git checkout production"
 alias gfd="git diff --name-status master.."
+
 # Nicely formatted diff for announcements
 alias deploydiff="git log production..master --pretty=format:'%<(23)%an    %s' --abbrev-commit"
-
-# Kubernetes
-# alias rollback-site="kubectl rollout undo deployment/site -n site"
-# alias rollback-graphql="kubectl rollout undo deployment/graphql -n site"
-# alias kstage="asdf shell kubectl 1.21.4 && asp staging && aws eks update-kubeconfig --name=oneflare-staging-v1 --alias=staging && kubectx staging"
-# alias kprod="asdf shell kubectl 1.20.4 && asp production && aws eks update-kubeconfig --name=oneflare-production-v1 --alias=production && kubectx production"
-
-# Boot Sidekiq locally
-# alias bsq="bundle exec sidekiq -C config/containers/sidekiq/sidekiq.yml"
 
 # iCloud dir
 alias ic='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
